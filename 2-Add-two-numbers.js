a73f2e3099611d8d8b17e40bd54f0282d4b9a172
tthanhthien <tthanhthien@savills.com.vn>
2024-02-18 21:52:28 +0700

    leetcode

--- 2-Add-two-numbers.js
+++ 2-Add-two-numbers.js
@@ -1,3 +1,6 @@
+// You are given two non - empty linked lists representing two non - negative integers.
+// The digits are stored in reverse order, and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -11,9 +14,7 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    const num1 = listToNumber(l1);
-    const num2 = listToNumber(l2);
-    return arrToList((num1 + num2).toString().split('').reverse());
+    return arrToList((listToNumber(l1) + listToNumber(l2)).toString().split('').reverse());
 };
 
 var listToNumber = function (l) {
